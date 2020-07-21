590b71dd875e497c23af0cd3a1e118c0ebc2d606
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-21 17:29:37 -0600

    Add routes for navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    get "/", to: "welcome#index"
-
+   get "/obituaries", to: "obituaries#index"
+   namespace :obituaries do
+     get 'covid-19', to: 'covid#index'
+   end
 end
