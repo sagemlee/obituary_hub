e43e73145b138d79a9b220af4baf749d7230da37
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-21 18:36:20 -0600

    Add more routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
    get "/obituaries", to: "obituaries#index"
    namespace :obituaries do
      get 'covid-19', to: 'covid#index'
+     get 'recent', to: 'recent#index'
+   end
+   get '/search', to: 'search#index'
+   namespace :search do
+     get 'advanced', to: 'advanced#index'
    end
 end
