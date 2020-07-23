53e2e7e02d7e666f123d174c69ae82da02c77281
Sage Lee <sagemcklee@gmail.com>
2020-07-23 15:29:54 -0600

    Update sessions controller with auth functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if request.env['omniauth.auth']
+      user = User.create_with_omniauth(request.env[‘omniauth.auth’])
+      session[:user_id] = user.id    
+      redirect_to "/profile" 
+    else 
       user = User.find_by(email: params[:email])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
@@ -12,7 +17,8 @@ class SessionsController < ApplicationController
       else
         flash[:notice] = "The credentials you have entered are invalid"
         redirect_to "/login"
-    end
+      end
+    end 
   end
 
   def destroy
