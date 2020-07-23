c0f552eaec20c794e4a88d8e6d7aa8a0b45fc80d
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-23 11:33:36 -0600

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+  Obituary.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    age: Faker::Number(within(range: 45..110)),
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    description: Faker::Lorem.paragraph(sentence_count: 5),
+    image_url: Faker::LoremFlicker.image(size: "50x60", search_terms: ['nature'])
+  )
+end 
