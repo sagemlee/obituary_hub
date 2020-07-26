6ad50c9f9dcb56107d5e08973f2b93eab808486c
Jack Cullen <jpcullen20@gmail.com>
2020-07-26 15:56:30 -0600

    Log in with facebook button now working with capybara

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
   scope: "public_profile", info_fields: "id,first_name,last_name,email"
--- spec/features/users/facebook_login_spec.rb
+++ spec/features/users/facebook_login_spec.rb
@@ -1,56 +1,24 @@
 require "rails_helper"
 
 describe "User logs in through facebook" do
-    before(:each) do
-        OmniAuth.config.mock_auth[:facebook] = nil
 
-        OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(
-            {"provider"=>"facebook",
-            "uid"=>"100626065081257",
-            "info"=>
-            {"email"=>"sharon_vzvvxsf_moidustein@tfbnw.net",
-            "first_name"=>"Sharon",
-            "last_name"=>"Moidustein",
-            "image"=>"http://graph.facebook.com/v3.0/100626065081257/picture"},
-            "credentials"=>
-            {"token"=>
-                "EAAqHzoceqSsBAN90pOElp2umzOEvZBNkOqtMzTL13oZBQMLpaZAH1QfXYanI9hLEmOsYvDhJOpYzEZCObeSagX8DOfiHx24yysYPfuiCjNccgCzZBnZApziZC7kZCPDUTVpqsfmNMWACdlLJeUZCmBXJ3y2j54LrZBZBz5dGliPYmd6RtGo6bv52P7GZBtmY3VmTCrAZD",
-            "expires_at"=>1600736355,
-            "expires"=>true},
-            "extra"=>
-            {"raw_info"=>
-                {"id"=>"100626065081257",
-                "first_name"=>"Sharon",
-                "last_name"=>"Moidustein",
-                "email"=>"sharon_vzvvxsf_moidustein@tfbnw.net"}
-            }})
-        end
-
-  xit "logs in through registration page" do
-        OmniAuth.config.test_mode = true
-
-        first_name = "Sharon"
-        last_name = "Moidustein"
-
+  it "logs in through registration page" do
         visit "/"
         click_on "Sign Up"
         expect(current_path).to eq("/users/new")
-        find('#facebook_sign_in').click
+        click_link('Sign in with Facebook')
 
         expect(current_path).to eq("/profile")
         expect(page).to have_content("You are now logged in as Sharon")
     end
 
-    xit "logs in through login page" do
-        OmniAuth.config.test_mode = true
-
-        first_name = "Sharon"
-        last_name = "Moidustein"
-
+    it "logs in through login page" do
         visit "/"
         click_on "Login"
         expect(current_path).to eq("/login")
-        find('#facebook_sign_in').click
+
+        click_link('Sign in with Facebook')
+        
         expect(current_path).to eq("/profile")
         expect(page).to have_content("You are now logged in as Sharon")
     end
