b6c0bd11fda8392a0bcc20791105a1df5409905f
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-25 20:55:02 -0600

    Add seeds for Covid and update

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,8 +19,22 @@ user = User.create!({
     age: Faker::Number.within(range: 45..110),
     city: Faker::Address.city,
     state: Faker::Address.state_abbr,
-    description: Faker::Lorem.paragraph(sentence_count: 10),
-    image_url: Faker::Fillmurray.image,
+    description: Faker::Lorem.paragraph(sentence_count: 15),
+    image_url: Faker::Fillmurray.unique.clear,
     user_id: user.id
   })
+
+  50.times do
+    Obituary.create!({
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      age: Faker::Number.within(range: 45..110),
+      city: Faker::Address.city,
+      state: Faker::Address.state_abbr,
+      description: Faker::Lorem.paragraph(sentence_count: 15),
+      image_url: Faker::Fillmurray.unique.clear,
+      covid: true,
+      user_id: user.id
+    })
+  end
 end
