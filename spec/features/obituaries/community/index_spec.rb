9b04325ba9b9e40bba5cf59e1ee67f02d830eb52
Sage Lee <sagemcklee@gmail.com>
2020-07-26 10:50:37 -0600

    Add community index page spec

---
+++ spec/features/obituaries/community/index_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe "As a visitor" do
+  it "I can see obituaries submitted by our user community" do
+
+    user = create(:user)
+    obituary = create(:obituary, user_id: user.id)
+    obituary2 = create(:obituary, user_id: user.id)
+    obituary3 = create(:obituary, user_id: user.id)
+
+    visit '/'
+
+    click_on "Community Obituaries"
+
+    expect(current_path).to eq('/obituaries/community')
+
+    expect(page).to have_content(obituary.first_name)
+    expect(page).to have_content(obituary.description)
+    expect(page).to_not have_content(obituary2.first_name)
+    expect(page).to_not have_content(obituary2.description)
+  end
+end
