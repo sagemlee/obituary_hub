00264883c1610ffc05ad9f9436f8622e7ae0e21c
Jack Cullen <jpcullen20@gmail.com>
2020-07-28 14:51:06 -0600

    Add ability to toggle results tables

--- app/controllers/search/community_controller.rb
+++ app/controllers/search/community_controller.rb
@@ -1,9 +1,5 @@
 class Search::CommunityController < ApplicationController
   def index
     @obituaries = Obituary.search_by_name(params[:name])
-    # respond_to do |format|
-    #   format.html
-    #   format.js
-    # end
   end
 end
