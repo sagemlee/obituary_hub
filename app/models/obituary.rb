a6ad6b0d43380dc3c1df02454f14912e71208c74
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-27 22:18:35 -0600

    Format and display community search results for name

--- app/models/obituary.rb
+++ app/models/obituary.rb
@@ -3,8 +3,25 @@ class Obituary < ApplicationRecord
   belongs_to :user
   before_save :default_image
 
+  def name
+    first_name + ' ' + last_name
+  end
+
+  def location
+    city + ' ' + state
+  end
+
+  def date
+    created_at.strftime("%B %-d, %Y")
+  end
+
   private
   def default_image
     self.image_url ||= 'https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg'
   end
+
+  def self.search_by_name(name)
+    Obituary.where("first_name ILIKE ? OR last_name ILIKE ?", "%#{name}%", "%#{name}%")
+  end
+
 end
