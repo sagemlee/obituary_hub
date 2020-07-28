bdf441e4a2a49315ab7fdb5216e3abf51904ddc1
Eric Larson <ericlarsonbroom@gmail.com>
2020-07-27 19:48:38 -0600

    Created test for recent obituaries from the api

---
+++ spec/features/obituaries/recent/index_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe "As a Visitor" do
+  it "I can see the recent obituaries from the api" do
+    visit '/'
+
+    click_on "Recently Published"
+
+    expect(current_path).to eq('/obituaries/recent')
+
+    within(first(".recent_obituary")) do
+
+      expect(page).to have_css(".headline")
+      headline = find(".headline").text
+      expect(headline).not_to be_empty
+
+      expect(page).to have_css(".abstract")
+      abstract = find(".abstract").text
+      expect(abstract).not_to be_empty
+
+      expect(page).to have_css(".web_url")
+      web_url = find(".web_url").text
+      expect(web_url).not_to be_empty
+
+      expect(page).to have_css(".pub_date")
+      pub_date = find(".pub_date").text
+      expect(pub_date).not_to be_empty
+
+      expect(page).to have_css(".word_count")
+      word_count = find(".word_count").text
+      expect(word_count).not_to be_empty
+    end
+  end
+end
