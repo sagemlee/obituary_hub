1e18357c3a6392f694a02b1b87fcc6661d8efab3
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-27 19:53:53 -0600

    Update wording of flash message

--- app/controllers/obituaries_controller.rb
+++ app/controllers/obituaries_controller.rb
@@ -11,7 +11,7 @@ class ObituariesController < ApplicationController
   def create
     @obituary = current_user.obituaries.new(obituary_params)
     if @obituary.save
-      flash[:success] = 'Obituary Created'
+      flash[:success] = 'New obituary was successfully created.'
       redirect_to obituary_path(@obituary.id)
     else
       flash[:error] = @obituary.errors.full_messages.to_sentence
