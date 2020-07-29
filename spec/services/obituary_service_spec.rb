930cf4d255eb4578f3fda18c86157423a8dbd715
Eric Larson <ericlarsonbroom@gmail.com>
2020-07-29 15:14:13 -0600

    Implemented caching

--- spec/services/obituary_service_spec.rb
+++ spec/services/obituary_service_spec.rb
@@ -21,4 +21,36 @@ describe "ObituaryService" do
 
     expect(obituaries.first[:data][:attributes][:abstract].downcase.include?('coronavirus') || obituaries.first[:data][:attributes][:abstract].downcase.include?('covid')).to eq(true)
   end
+
+  it "returns recent obituaries", :vcr do
+    service = ObituaryService.new
+    obituaries = service.get_recent_obituaries
+
+    expect(obituaries).to be_a Array
+    expect(obituaries.first).to be_a Hash
+
+    expect(obituaries.first[:data][:type]).to eq('obituary')
+  end
+
+  it "returns advanced obituaries", :vcr do
+    service = ObituaryService.new
+    name = "George"
+    year = 2019
+    obituaries = service.get_advanced_obituaries(name, year)
+
+    expect(obituaries).to be_a Array
+    expect(obituaries.first).to be_a Hash
+    expect(obituaries.first[:data][:type]).to eq('obituary')
+    expect(obituaries.first[:data][:attributes][:headline].include?('George'))
+  end
+
+  it "returns obituaries by name", :vcr do
+    service = ObituaryService.new
+    name = "George"
+    obituaries = service.get_obituaries_by_name(name)
+    expect(obituaries).to be_a Array
+    expect(obituaries.first).to be_a Hash
+    expect(obituaries.first[:data][:type]).to eq('obituary')
+    expect(obituaries.first[:data][:attributes][:headline].include?('George'))
+  end
 end
