852f3b90d01743cbc70e5b255b14a5dbcfd980c9
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-29 21:02:08 -0600

    Change classes to ids

--- spec/features/search/results/index_spec.rb
+++ spec/features/search/results/index_spec.rb
@@ -49,31 +49,29 @@ describe "As a visitor" do
 
     expect(current_path).to eq('/search/results')
 
-    within(first(".advanced_obituary")) do
+    within(first("#obituary_by_name")) do
 
-      expect(page).to have_css(".headline")
-      headline = find(".headline").text
+      expect(page).to have_css("#headline")
+      headline = find("#headline").text
       expect(headline).not_to be_empty
 
-      expect(page).to have_css(".abstract")
-      abstract = find(".abstract").text
+      expect(page).to have_css("#abstract")
+      abstract = find("#abstract").text
       expect(abstract).not_to be_empty
 
-      expect(page).to have_css(".web_url")
-      web_url = find(".web_url").text
+      expect(page).to have_css("#web_url")
+      web_url = find("#web_url").text
       expect(web_url).not_to be_empty
 
-      expect(page).to have_css(".pub_date")
-      pub_date = find(".pub_date").text
-      expect(pub_date).to have_content("2019")
+      expect(page).to have_css("#pub_date")
+      pub_date = find("#pub_date").text
+      expect(pub_date).not_to be_empty
 
-      expect(page).to have_css(".word_count")
-      word_count = find(".word_count").text
+      expect(page).to have_css("#word_count")
+      word_count = find("#word_count").text
       expect(word_count).not_to be_empty
 
-      expect(page).to have_css(".image")
-      image = find(".image").text
-      expect(image).not_to be_empty
+      expect(page).to have_css("#image")
     end
   end
 
