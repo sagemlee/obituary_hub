f5d0cd59d914c1dfb37cc0298d5ca0e4fcb8e5bc
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-30 15:03:44 -0600

    Comment out faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create!({
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: "example@email.com",
-    password: "password_digest"
-  })
+# user = User.create!({
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: "example@email.com",
+#     password: "password_digest"
+#   })
 #
 # 100.times do
 #   Obituary.create!({
