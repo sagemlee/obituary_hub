9a8abef5d7f491251870f1cc15766611968b60cc
Jack Cullen <jpcullen20@gmail.com>
2020-07-30 12:03:52 -0600

    Update microservice routes

--- app/services/obituary_service.rb
+++ app/services/obituary_service.rb
@@ -5,24 +5,24 @@ class ObituaryService
   end
 
   def get_covid_obituaries
-    resp = conn.get('/covid')
+    resp = conn.get('/api/v1/covid')
     JSON.parse(resp.body, symbolize_names: true)
   end
 
   def get_obituaries_by_name(name)
-    resp = conn.get('/name') do |req|
+    resp = conn.get('/api/v1/name') do |req|
       req.params['name'] = "#{name}"
     end
     JSON.parse(resp.body, symbolize_names: true)
   end
 
   def get_recent_obituaries
-    resp = conn.get('/recent')
+    resp = conn.get('/api/v1/recent')
     JSON.parse(resp.body, symbolize_names: true)
   end
 
   def advanced_search_name_and_year(name, year)
-    resp = conn.get('/advanced') do |req|
+    resp = conn.get('/api/v1/advanced') do |req|
       req.params['name'] = "#{name}"
       req.params['date'] = year.to_i
     end
@@ -30,7 +30,7 @@ class ObituaryService
   end
 
   def advanced_search_year(year)
-    resp = conn.get('/advanced/year') do |req|
+    resp = conn.get('/api/v1/advanced/year') do |req|
       req.params['date'] = year.to_i
     end
     JSON.parse(resp.body, symbolize_names: true)
